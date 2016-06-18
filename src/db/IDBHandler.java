c840d877e9707adf4d9f58b6a12c71d7dd0b5d54
wappm <e1325702@student.tuwien.ac.at>
2016-06-19 00:11:50 +0200

    Moved some stuff, added some stuff

---
+++ src/db/IDBHandler.java
@@ -0,0 +1,7 @@
+package db;
+
+/**
+ * Created by Michael on 18/06/16.
+ */
+public interface IDBHandler {
+}
---
+++ src/service/HashService.java
@@ -0,0 +1,7 @@
+package service;
+
+/**
+ * Created by Michael on 18/06/16.
+ */
+public class HashService {
+}
